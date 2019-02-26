2dfd7c0c4de6ac61cf7087c1d5ce48f3052efefa
Superdizzy17 <dylankieran@hotmail.com>
2019-02-26 14:39:33 +0000

    Mike and Dave Need Wedding Dates

--- src/youtube.js
+++ src/youtube.js
@@ -7,6 +7,73 @@ document.getElementById("back").href = "http://localhost:8081/?access_token=" + 
 
 console.log(YouTubeKey);
 
+//Function to retrieve top songs
+function topSongs(YouTubeKey)
+{
+    return fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet'
+    + '&maxResults=10'
+    + '&chart=mostPopular' 
+    + '&videoCategoryId=10'
+    + '&regionCode=IE'
+    + '&key=' + YouTubeKey,{
+        method: 'get'
+    })
+    .then(res =>  res.json())
+    .catch(error => console.log(error))
+}
+
+//Populate div with top songs 
+topSongs(YouTubeKey).then(function(res) {
+
+    let id = 0;
+    console.log(res);
+    document.getElementById("TopSongs").innerHTML = null;
+
+    for(let i=0; i<10; i++)
+    {
+        if(res.items[i].kind === "youtube#video")
+        {
+           document.getElementById("TopSongs").innerHTML += 
+            "<div class=\"result\"id=\"item" + i + "\">" 
+            + "<img class=\"thumbnail\"id=\"thumbnail" + id + "\"" + "src=\""
+            + res.items[i].snippet.thumbnails.default.url + 
+            "\"" + "onclick=\"playYoutube('" + res.items[i].id + "')\">" 
+            + "<h1>"+ res.items[i].snippet.title + "</h1>"
+            + "</div>";
+            id++;
+        }
+    }
+})
+
+//Add video to player div
+window.playYoutube = function(videoId)
+{
+    document.getElementById("player").innerHTML = null
+
+    document.getElementById("player").innerHTML = "<iframe class=\"youtubePlayer\"" +
+    "src=\"https://www.youtube.com/embed/" + videoId + "?autoplay=1\">" +
+    "</iframe>";
+}
+
+
+//YouTube Search Function
+function youtubeSearch(YouTubeKey, youtubeQuery, maxResults)
+{
+    let q = youtubeQuery.split(' ').join('+');
+    let arrayRes = [];
+    let id = 0;
+
+    return fetch('https://www.googleapis.com/youtube/v3/search?part=snippet'
+    + '&maxResults=' + maxResults
+    + '&q=' + q
+    + '&key=' + YouTubeKey,{
+        method: 'get'
+    })
+    .then(res =>  res.json())
+    .catch(error => console.log)
+}
+
+//Populate div with search results
 document.getElementById("searchYoutube").onclick = function()
 {
     let youtubeQuery = document.getElementById("youtubeQuery").value;
@@ -33,28 +100,3 @@ document.getElementById("searchYoutube").onclick = function()
         }
     })
 };
-
-function youtubeSearch(YouTubeKey, youtubeQuery, maxResults)
-{
-    let q = youtubeQuery.split(' ').join('+');
-    let arrayRes = [];
-    let id = 0;
-
-    return fetch('https://www.googleapis.com/youtube/v3/search?part=snippet'
-    + '&maxResults=' + maxResults
-    + '&q=' + q
-    + '&key=' + YouTubeKey,{
-        method: 'get'
-    })
-    .then(res =>  res.json())
-    .catch(error => console.log)
-}
-
-window.playYoutube = function(videoId)
-{
-    document.getElementById("player").innerHTML = null
-
-    document.getElementById("player").innerHTML = "<iframe class=\"youtubePlayer\"" +
-    "src=\"https://www.youtube.com/embed/" + videoId + "?autoplay=1\">" +
-    "</iframe>";
-}
